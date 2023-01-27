01feb3ffe465fb3c4928ed24e48f26da572294b5
Sanjaiyan <parthipankalayini@gmail.com>
2023-01-28 02:11:48 +0530

    Initialization 🛠

---
+++ sanpasjs-wesite/astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react(), mdx(), tailwind()]
+});
