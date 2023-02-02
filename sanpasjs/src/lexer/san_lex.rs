064524b6d1fa1cebda2321269c1e6df44ffc1a0f
Sanjaiyan <parthipankalayini@gmail.com>
2023-02-02 21:30:07 +0530

    Add token match statements ⚒

--- sanpasjs/src/lexer/san_lex.rs
+++ sanpasjs/src/lexer/san_lex.rs
@@ -1,10 +1,5 @@
 use logos::{Lexer, Logos};
-use std::{
-    borrow::{Borrow, BorrowMut},
-    fs,
-    mem::take,
-    process,
-};
+use std::{fs, process};
 
 use super::san_tokens::SanTokenKinds;
 
@@ -41,7 +36,7 @@ impl SanjaiyanPascalCode {
     ) -> (bool, SanTokenKinds) {
         if let Some(sanjaiyan_pos_token) = sanjaiyan_token_collections.nth(pos) {
             if sanjaiyan_pos_token == san_token_to_check {
-                return (true, san_token_to_check);
+                (true, san_token_to_check)
             } else {
                 (false, san_token_to_check)
             }
@@ -55,11 +50,29 @@ impl SanjaiyanPascalCode {
 
         let mut san_tokens_collection = self.san_tokenize();
 
-        for san_token in san_tokens_collection.clone().enumerate() {
-            println!("{:?}", san_token.1);
+        for (current_san_pos, current_san_token) in san_tokens_collection.clone().enumerate() {
+            match current_san_token {
+                SanTokenKinds::PascalProgramStart => todo!(),
+                SanTokenKinds::PascalProgramEnd => todo!(),
+                SanTokenKinds::PascalCodeBlockBegin => todo!(),
+                SanTokenKinds::PascalCodeBlockEnd => todo!(),
+                SanTokenKinds::LetDeclare => todo!(),
+                SanTokenKinds::ConstDeclare => todo!(),
+                SanTokenKinds::AssignVar => todo!(),
+                SanTokenKinds::DataType(_) => todo!(),
+                SanTokenKinds::LeftParen => todo!(),
+                SanTokenKinds::RightParen => todo!(),
+                SanTokenKinds::SemiColon => todo!(),
+                SanTokenKinds::Colon => todo!(),
+                SanTokenKinds::Comma => todo!(),
+                SanTokenKinds::AndOp => todo!(),
+                SanTokenKinds::OrOp => todo!(),
+                SanTokenKinds::Indentifier(_) => todo!(),
+                SanTokenKinds::Number(_) => todo!(),
+                SanTokenKinds::Text(_) => todo!(),
+                SanTokenKinds::SanError => todo!(),
+            }
         }
-
-        print!("{:?}", san_tokens_collection);
 
         san_organized_tokens
     }
